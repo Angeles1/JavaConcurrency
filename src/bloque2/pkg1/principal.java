517ed435ec3a0bd1e565b67fbd1b6755bff8c588
Angeles <angeles.fuent.exp@gmail.com>
2017-04-30 12:10:49 +0100

    Tutorial_2

--- src/bloque2/pkg1/principal.java
+++ src/bloque2/pkg1/principal.java
@@ -5,7 +5,7 @@ package bloque2.pkg1;
 import java.util.Random;
 
 public class principal extends Thread {
-    private static int tam = 4;
+    private static int tam = 800;
     private static int[][] matriz = new int[tam][tam];
     int ini, fin;
     //constructor
@@ -36,15 +36,22 @@ public class principal extends Thread {
         //empiezo a medir el tiempo
         tiempo_inicio= System.nanoTime(); //obtenemos el hora actual en nanosegundos
         
-        principal h1 = new principal(0,2);
-        principal h2 = new principal(2,matriz.length);
+        principal h1 = new principal(0,100);
+        principal h2 = new principal(400,matriz.length);
+        principal h3 = new principal(100,200);
+        principal h4= new principal(200,300);
+        principal h5= new principal(300,400);
         
         h1.start();
         h2.start();
+        h3.start();
+        h4.start();
         
         try{
             h1.join();
             h2.join();
+            h3.join();
+            h4.join();
         }catch(Exception e){}
         
         //tiempo que ha tardado en ejecutar la concurrencia
@@ -52,12 +59,12 @@ public class principal extends Thread {
         System.out.println((tiempo_final/1000000)+" milisegundos");
         
         //mostrar la matriz con el hilo principal cuando ya se ha modificado con los otros hilos
-        for(int i=0; i<matriz.length;i++){
+        /*for(int i=0; i<matriz.length;i++){
             for (int j=0; j<matriz[0].length;j++){
                 System.out.print(matriz[i][j]+" ");
             }
             System.out.println();
-        }
+        }*/
                 
     }
     
