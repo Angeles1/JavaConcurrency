6ec4efaec05eda9f8353822fade5941c7d27091d
Angeles <angeles.fuent.exp@gmail.com>
2017-04-30 13:02:34 +0100

    Tutorial_3.1

--- src/bloque2/pkg1/principal.java
+++ src/bloque2/pkg1/principal.java
@@ -4,14 +4,20 @@ public class principal implements Runnable{
     
  
     public static void main(String[] args) {
-      Runnable runnable = new principal();
-      Thread hilo1 = new Thread(runnable);
-      hilo1.start();
+      Thread[] hilos = new Thread[2];
       
-      try{
-          hilo1.join();
-      }catch (Exception e){}
-
+      for (int i =0; i<hilos.length;i++){
+          Runnable runnable= new principal();
+          hilos[i]=new Thread(runnable);
+          hilos[i].start();
+      }
+      
+      for(int i=0; i<hilos.length;i++){
+        try{
+            hilos[i].join(); 
+        }catch (Exception e){}
+        
+        }
     }
 
     @Override
