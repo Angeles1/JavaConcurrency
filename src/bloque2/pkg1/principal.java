986f583540c750ff01fab2f9d92296de9ce5849c
Luis Zoran <luiszoran@gmail.com>
2017-04-30 11:15:36 +0100

    Tutorial_1

--- src/bloque2/pkg1/principal.java
+++ src/bloque2/pkg1/principal.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bloque2.pkg1;
 
 import java.util.Random;
 
-/**
- *
- * @author LUISZORANROBAINAKOTU
- */
 public class principal extends Thread {
     //declaracion del vector estatico para que varios hilos accedan a el
     private static int tam=8;
